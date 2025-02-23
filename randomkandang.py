e928580067935c1d35a2770903fed156f2de75ac
adiorany3 <158135665+adiorany3@users.noreply.github.com>
2025-02-23 22:48:25 +0700

    tambah FCR

--- randomkandang.py
+++ randomkandang.py
@@ -164,7 +164,7 @@ if st.sidebar.button("Hitung Indeks Performans"):
             ADG_actual = ((Total_Body_Weight/Live_Bird)*1000) if Live_Bird > 0 else 0  # Calculate ADG_actual, avoid division by zero
             feed = FCR * Total_Body_Weight
             new_data = pd.DataFrame([[Age, today, Total_Body_Weight, FCR, Live_Bird, Ayam_Dipelihara, persen_Live_Bird, actual_ip, prediction, culling, ADG_actual, feed]],
-                                    columns=['Age', 'Date', 'Total_Body_Weight', 'Live_Bird', 'Ayam_Dipelihara', 'persen_Live_Bird', 'IP_actual', 'IP', 'Culling', 'ADG_actual', 'Feed', 'FCR_actual'])
+                                    columns=['Age', 'Date', 'Total_Body_Weight', 'FCR', 'Live_Bird', 'Ayam_Dipelihara', 'persen_Live_Bird', 'IP_actual', 'IP', 'Culling', 'ADG_actual', 'Feed', 'FCR_actual'])
             try:
                 existing_data = pd.read_csv('prediksi.csv')
 
@@ -178,7 +178,7 @@ if st.sidebar.button("Hitung Indeks Performans"):
                     existing_data.rename(columns={'FCR': 'FCR_actual'}, inplace=True)
 
                 # Define the desired column order
-                column_order = ['Age', 'Date', 'Total_Body_Weight', 'Live_Bird', 'Ayam_Dipelihara', 'persen_Live_Bird', 'IP_actual', 'IP', 'Culling', 'ADG_actual', 'Feed', 'FCR_actual']
+                column_order = ['Age', 'Date', 'Total_Body_Weight', 'FCR', 'Live_Bird', 'Ayam_Dipelihara', 'persen_Live_Bird', 'IP_actual', 'IP', 'Culling', 'ADG_actual', 'Feed', 'FCR_actual']
 
                 # Reorder the columns, handling missing columns
                 existing_columns = existing_data.columns.tolist()
